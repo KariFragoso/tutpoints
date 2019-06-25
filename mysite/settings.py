05e03de86e129411cbe5ce4ed6a1d3b38ebec2b7
Karine <Karine@exemplo.com>
2019-06-25 02:52:06 -0300

    correção html

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'karileti.fragoso0@gmail.com' 
-EMAIL_HOST_PASSWORD = 'zughazqzqnqpibhn'
+EMAIL_HOST_USER = '0@gmail.com' 
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
